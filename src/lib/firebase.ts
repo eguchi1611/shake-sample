ae65d87a78d8982082f6efe8664140b78ace8c47
Eguchi <eguchi@eguchi.cc>
2023-06-25 11:46:51 +0900

    カウント共有の実装

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getDatabase(app);
+export const auth = getAuth(app);
--- src/pages/counter.tsx
+++ src/pages/counter.tsx
@@ -1,14 +1,87 @@
 import { useShake } from "@/hooks/use-shake";
-import { useCallback, useState } from "react";
+import { auth, db } from "@/lib/firebase";
+import { Unsubscribe, User, signInAnonymously } from "firebase/auth";
+import { onValue, ref, set } from "firebase/database";
+import { useCallback, useEffect, useState } from "react";
 
 export default function AppPage() {
   const [count, setCount] = useState(0);
+  const [sumCount, setSumCount] = useState(0);
+  const [status, setStatus] = useState<Record<string, string>>({});
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setStatus((c) => ({
+      ...c,
+      auth: "ログイン中",
+    }));
+    signInAnonymously(auth)
+      .then((credential) => {
+        setStatus((c) => ({
+          ...c,
+          auth: "匿名ユーザー: " + credential.user.uid + " でログインしました",
+        }));
+        setUser(credential.user);
+      })
+      .catch(() => {
+        setStatus((c) => ({
+          ...c,
+          auth: "ログインに失敗しました",
+        }));
+      });
+  }, []);
 
   const onShake = useCallback(() => {
-    setCount((c) => c + 1);
-  }, []);
+    if (user) {
+      set(ref(db, "counter/" + user.uid), {
+        count: count + 1,
+      });
+    }
+  }, [user, count]);
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    if (user) {
+      unsubscribe = onValue(
+        ref(db, "counter/" + user.uid + "/count"),
+        (snapshot) => {
+          const data = snapshot.val();
+          setCount(data);
+        }
+      );
+    }
+    return () => {
+      if (unsubscribe !== null) unsubscribe();
+    };
+  }, [count, user]);
+
+  useEffect(() => {
+    const unsubscribe = onValue(ref(db, "counter"), (snapshot) => {
+      const data = snapshot.val();
+      setStatus((c) => ({
+        ...c,
+        database: data,
+      }));
+      setSumCount(
+        Object.values(data)
+          .map((v: any) => v.count)
+          .reduce((p, c) => p + c, 0)
+      );
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [count, user]);
 
   useShake(10, onShake);
 
-  return <div>{count}</div>;
+  return (
+    <div>
+      <div>ステータス</div>
+      <pre>{JSON.stringify(status, null, 2)}</pre>
+      <div>現在のあなたのカウント: {count}</div>
+      <div>全員の合計カウント: {sumCount}</div>
+    </div>
+  );
 }
