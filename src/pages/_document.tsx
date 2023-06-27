7eab5f2ad5b4c28b868377592becbda32e7ad335
Eguchi <eguchi@eguchi.cc>
2023-06-27 12:03:11 +0900

    スタイルの調整

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link
+          href="https://use.fontawesome.com/releases/v6.4.0/css/all.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
